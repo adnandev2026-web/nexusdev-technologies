d087d610bb550c4f54402c31f13e8e42c36b8d1d
ADNAN AHMAD A P <adnandev2026@gmail.com>
2026-09-14 06:04:48 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = "nexusdev-v1";
+const FILES = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
+});
